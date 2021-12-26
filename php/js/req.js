ba47dbf0050474246a7197c8e3240073019dbdf8
Marty <39444350+MartyLocke@users.noreply.github.com>
2021-12-26 20:35:24 +0100

    dbDropSession
    Pozwala usunac / wygasic obecna sesje

--- php/js/req.js
+++ php/js/req.js
@@ -3,8 +3,6 @@
 */
 
 // callback (json) => resolved json for command, and packed data
-
-
 const dbReq = function (callback, command, packed = [])
 {
   const localToken = localStorage.getItem("token");
@@ -16,10 +14,27 @@ const dbReq = function (callback, command, packed = [])
 
   for (let i = 0; i < packed.length; i += 2)
     reqUrl += `&${packed[i]}=${packed[i + 1]}`;
-    
+
+  console.log(reqUrl);
+
   fetch(new Request(reqUrl))
   .then(response => response.json())
   .then(json => {
     callback(json);
   });
+}
+
+const dbDropSession = function ()
+{
+  dbReq((e) => {
+    if (e.success)
+    {
+      localStorage.clear();
+      window.location.href = './index.html';
+    }
+    else
+      alert(
+        `Wylogowywanie nie powiodlo sie: ${e.err}`
+      );
+  }, "dropSess", []);
 }
