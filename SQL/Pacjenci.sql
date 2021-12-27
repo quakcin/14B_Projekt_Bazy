4e9fe6fb2e48869093ae94a14f0280a2c553c17f
bandurama <95710274+bandurama@users.noreply.github.com>
2021-12-27 17:54:14 +0100

    Dodatkowy podział na pliki.

---
+++ SQL/Pacjenci.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE VIEW Pacjenci_view AS
+SELECT  Konta.login, Konta.haslo, osoby.imie, osoby.nazwisko, osoby.data_urodzenia, osoby.pesel, kontakty.telefon, 
+        kontakty.email, adresy.miasto, adresy.ulica, adresy.nr_domu, adresy.nr_mieszkania, 
+        adresy.kod_pocztowy, osoby.nr_osoby
+        FROM Osoby
+        INNER JOIN Adresy ON osoby.adres_nr = adresy.nr_adresu
+        INNER JOIN Kontakty ON osoby.kontakt_nr = kontakty.nr_kontaktu
+        INNER JOIN Konta ON Osoby.Nr_osoby=Konta.Osoba_Nr;
+        
+
+CREATE OR REPLACE TRIGGER Pacjent_add_trigger
+INSTEAD OF INSERT ON Pacjenci_view
+FOR EACH ROW
+BEGIN
+    INSERT INTO Adresy VALUES (ADRESY_SEQUENCE.nextval, :NEW.Miasto, :NEW.Ulica, :NEW.Nr_Domu, :NEW.Nr_Mieszkania, :NEW.Kod_Pocztowy);
+    INSERT INTO Kontakty VALUES (KONTAKTY_SEQUENCE.nextval, :NEW.Telefon, :NEW.Email);
+    INSERT INTO Osoby VALUES (OSOBY_SEQUENCE.nextval, :NEW.Nazwisko, :NEW.Imie, :NEW.Data_Urodzenia, :NEW.PESEL, ADRESY_SEQUENCE.currval, KONTAKTY_SEQUENCE.currval);
+    INSERT INTO Pacjenci VALUES (PACJENCI_SEQUENCE.nextval, OSOBY_SEQUENCE.currval);
+    INSERT INTO Konta VALUES (KONTA_SEQUENCE.nextval, :NEW.Login, :NEW.Haslo, 'pacjent', OSOBY_SEQUENCE.currval);
+END;
+/
