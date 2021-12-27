a0fe393b19134e38945746220a050a89cae54bd0
MrMarioo <95710806+MrMarioo@users.noreply.github.com>
2021-12-27 12:20:01 +0100

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!--
+  Mini Projekt Baz Danych
+  2ID14B, Przychodnia
+-->
+<html lang="pl">
+  <head>
+		<link rel="stylesheet" href="css/m_style.css">
+  </head>
+  <body>
+	<div class="m_head">
+		<input type="button" value="Info" id="btn1">
+		
+		<script src="js/req.js"></script>
+		
+
+		<!-- Podmień zaloguj z Moje Konto w przypadku zalogowania -->
+		<!-- Zrub wrapper do logowania w php -->
+		<!-- Dodaj loggera do js'a zeby dodawal tokena do linkow -->
+		<!-- Wrapper moze obejmowac cale menu -->
+	<div class="header">
+	  <h1>Apteka</h1>
+	  <p><b>LOOOOREM</b> Ipsum ipsum.</p>
+	</div>
+	<div class="navbar">
+		<a href="logowanie.php">Zaloguj</a> 
+		<a href="rejestracja.php">Zarejestruj Sie</a> 
+		<a href="konto.php">Moje Konto</a> 
+		<a href="wizyty.php"> Umuw sie na wizyte </a>
+		<a href="informator.php"> Informacje o Pacjencie / Lekarzu </a>
+		<a href="apteka.php"> Apteka </a>
+	</div>
+		
+	<div class="row">
+		<div class="side">
+			<h2>O aptece</h2>
+			<h5>Przykladowe zdjecia:</h5>
+			<div class="fakeimg" style="height:200px;">Image</div>
+			<p> mea culpa qui officia deserunt mollit anim..</p>
+			<h3>More Text</h3>
+			<p>Lorem ipsum dolor sit ame.</p>
+			<div class="fakeimg" style="height:60px;">Image</div><br>
+			<div class="fakeimg" style="height:60px;">Image</div><br>
+			<div class="fakeimg" style="height:60px;">Image</div>
+		</div>
+		<div class="main">
+			<h2>TITLE HEADING</h2>
+			<h5>Title description, Dec 27, 2021</h5>
+			<div class="fakeimg" style="height:200px;">Image</div>
+			<p>Some text..</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+			<br>
+			<h2>TITLE HEADING</h2>
+			<h5>Title description, Dec 27, 2021</h5>
+			<div class="fakeimg" style="height:200px;">Image</div>
+			<p>Some text..</p>
+			<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+	  </div>
+	</div>
+
+<div class="footer">
+  <h2>Footer</h2>
+</div>
+	</div>
+  </body>
+</html>
