b412704a29e01cfd77a60d389d48d1de30e976f0
bandurama <95710274+bandurama@users.noreply.github.com>
2021-12-29 20:57:40 +0100

    Dodano procedurę edycji pacjenta.

--- SQL/Pacjenci.sql
+++ SQL/Pacjenci.sql
@@ -20,3 +20,18 @@ BEGIN
     INSERT INTO Konta VALUES (KONTA_SEQUENCE.nextval, :NEW.Login, :NEW.Haslo, 'pacjent', OSOBY_SEQUENCE.currval);
 END;
 /
+
+CREATE OR REPLACE PROCEDURE Pacjent_Update (p_imie osoby.imie%TYPE, p_nazwisko osoby.nazwisko%TYPE, p_haslo konta.haslo%TYPE, p_data_uro osoby.data_urodzenia%TYPE, 
+                                            p_pesel osoby.pesel%TYPE, p_telefon kontakty.telefon%TYPE, p_email kontakty.email%TYPE, p_miasto adresy.miasto%TYPE,
+                                            p_ulica adresy.ulica%TYPE, p_dom adresy.nr_domu%TYPE, p_mieszk adresy.nr_mieszkania%TYPE, p_Kod_Poczt adresy.kod_pocztowy%TYPE, 
+                                            p_Nr_Osoby Osoby.nr_osoby%TYPE)
+IS
+BEGIN
+    UPDATE Adresy   SET Miasto = p_miasto, Ulica = p_ulica, Nr_Domu = p_dom, nr_mieszkania = p_mieszk, kod_pocztowy = p_Kod_Poczt WHERE Nr_Adresu = (SELECT Adres_Nr FROM Osoby WHERE Nr_Osoby =  p_Nr_Osoby);
+    UPDATE Kontakty SET Telefon = p_telefon, Email = p_email WHERE Nr_kONTAKTU = (SELECT Kontakt_Nr FROM Osoby WHERE Nr_Osoby =  p_Nr_Osoby);
+    UPDATE Osoby    SET Nazwisko = p_nazwisko, Imie = p_imie, Data_Urodzenia = p_data_uro, PESEL = p_pesel WHERE Nr_Osoby = p_Nr_Osoby;
+    UPDATE Konta    SET haslo = p_haslo WHERE Osoba_Nr = p_Nr_Osoby;
+END;
+/
+
+--EXECUTE Pacjent_Update('Tomasz', 'Boniek', 'qwerty123', sysdate-14700, '13785236985', NULL, NULL, 'Warszawa', 'Solidarności', '1', NULL, '25-255', 3);
