1fe25cf94c8422114cdaad758af51aceecb0bc76
ObalonyPiwot <74457736+ObalonyPiwot@users.noreply.github.com>
2021-12-29 22:54:47 +0100

    Add files via upload

--- SQL/Pacjenci.sql
+++ SQL/Pacjenci.sql
@@ -10,14 +10,29 @@ SELECT  Konta.login, Konta.haslo, osoby.imie, osoby.nazwisko, osoby.data_urodzen
         
 
 CREATE OR REPLACE TRIGGER Pacjent_add_trigger
-INSTEAD OF INSERT ON Pacjenci_view
+INSTEAD OF INSERT OR UPDATE OR DELETE ON Pacjenci_view
 FOR EACH ROW
 BEGIN
+CASE
+WHEN INSERTING THEN
     INSERT INTO Adresy VALUES (ADRESY_SEQUENCE.nextval, :NEW.Miasto, :NEW.Ulica, :NEW.Nr_Domu, :NEW.Nr_Mieszkania, :NEW.Kod_Pocztowy);
     INSERT INTO Kontakty VALUES (KONTAKTY_SEQUENCE.nextval, :NEW.Telefon, :NEW.Email);
     INSERT INTO Osoby VALUES (OSOBY_SEQUENCE.nextval, :NEW.Nazwisko, :NEW.Imie, :NEW.Data_Urodzenia, :NEW.PESEL, ADRESY_SEQUENCE.currval, KONTAKTY_SEQUENCE.currval);
     INSERT INTO Pacjenci VALUES (PACJENCI_SEQUENCE.nextval, OSOBY_SEQUENCE.currval);
     INSERT INTO Konta VALUES (KONTA_SEQUENCE.nextval, :NEW.Login, :NEW.Haslo, 'pacjent', OSOBY_SEQUENCE.currval);
+WHEN UPDATING THEN
+    UPDATE Adresy SET Miasto = :NEW.Miasto, Ulica = :NEW.Ulica, Nr_Domu = :NEW.Nr_Domu, Nr_Mieszkania = :NEW.Nr_Mieszkania, Kod_Pocztowy = :NEW.Kod_Pocztowy WHERE Nr_Adresu = (SELECT Adres_Nr FROM Osoby WHERE Nr_Osoby = :NEW.Nr_Osoby);
+    UPDATE Kontakty SET Telefon = :NEW.Telefon, Email = :NEW.Email WHERE Nr_Kontaktu = (SELECT Kontakt_Nr FROM Osoby WHERE Nr_Osoby = :NEW.Nr_Osoby); 
+    UPDATE Osoby SET Nazwisko = :NEW.Nazwisko, Imie = :NEW.Imie, Data_Urodzenia = :NEW.Data_Urodzenia, PESEL = :NEW.PESEL WHERE Nr_Osoby = :NEW.Nr_Osoby;
+    UPDATE Konta SET Haslo = :NEW.Haslo WHERE Osoba_Nr = :NEW.Nr_Osoby;
+WHEN DELETING THEN
+    DELETE FROM Pacjenci WHERE Osoba_Nr = :OLD.Nr_Osoby;
+    DELETE FROM Konta WHERE Osoba_Nr = :OLD.Nr_Osoby;
+    DELETE FROM Sesje WHERE Osoba_Nr = :OLD.Nr_Osoby;
+    DELETE FROM Osoby WHERE Nr_Osoby = :OLD.Nr_Osoby; 
+    DELETE FROM Adresy WHERE Nr_Adresu = (SELECT Adres_Nr FROM Osoby WHERE Nr_Osoby = :OLD.Nr_Osoby);
+    DELETE FROM Kontakty WHERE Nr_Kontaktu = (SELECT Kontakt_Nr FROM Osoby WHERE Nr_Osoby = :OLD.Nr_Osoby); 
+END CASE;
 END;
 /
 
@@ -33,5 +48,6 @@ BEGIN
     UPDATE Konta    SET haslo = p_haslo WHERE Osoba_Nr = p_Nr_Osoby;
 END;
 /
-
+select * from pacjenci_view;
+DELETE Pacjenci_view where Nr_Osoby = 2;
 --EXECUTE Pacjent_Update('Tomasz', 'Boniek', 'qwerty123', sysdate-14700, '13785236985', NULL, NULL, 'Warszawa', 'Solidarności', '1', NULL, '25-255', 3);
