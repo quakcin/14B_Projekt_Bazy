b7fdd82b1a3eb64e5eefabe560ab83502d65cd3e
bandurama <banduram1@gmail.com>
2022-01-21 00:26:22 +0100

    (SQL) SuperTurboGenratorWizty()

--- SQL/wizyty.sql
+++ SQL/wizyty.sql
@@ -96,27 +96,55 @@ godziny NVARCHAR2(80);
 data_wizyty NVARCHAR2(80);
 los NUMBER;
 prawdopodob NUMBER;
+type Opisarray IS VARRAY(50) OF VARCHAR2(256); 
+ZaplanowaneOpis Opisarray;
+OdwolaneOpis Opisarray;
+OdbyteOpis Opisarray;
+PrzeniesioneOpis Opisarray;
+Zalecenia Opisarray;
 BEGIN
-FOR i IN 1..p_ilosc LOOP
-    los:=round(dbms_random.VALUE(-100, 60));
-    SELECT to_char(sysdate + los, 'DD.MM.YYYY') INTO dni FROM dual;
-    SELECT * INTO godziny FROM(SELECT * FROM dostepne_godz ORDER BY DBMS_RANDOM.RANDOM) WHERE  ROWNUM < 2;
-    data_wizyty := (dni||' '||godziny);
-    INSERT INTO Wizyty (lekarz_nr, pacjent_nr, Data_wizyty, czy_odbyta) 
-    VALUES(round(DBMS_RANDOM.VALUE(1,(SELECT MAX(Nr_Lekarza) FROM Lekarze))),round(DBMS_RANDOM.VALUE(1,(SELECT MAX(Nr_Karty_Pacjenta) FROM Pacjenci))), TO_DATE(data_wizyty,'DD.MM.YYYY HH24:MI'), 'Zaplanowana');
-    IF(dbms_random.VALUE(0,100)<80 AND los<0) THEN
-        INSERT INTO Recepty (Wizyta_Nr) VALUES ((SELECT MAX(NR_Wizyty) FROM Wizyty));
-        prawdopodob:=round(dbms_random.VALUE(1,4));
-        FOR i IN 1..prawdopodob LOOP
-            INSERT INTO Lek_Na_Recepte VALUES ((SELECT MAX(NR_Recepty) FROM Recepty), (round(dbms_random.VALUE(1,(SELECT MAX(NR_LEKU) FROM LEKI)))));
-        END LOOP;
-    END IF;
-    IF(dbms_random.VALUE(0,100) < 3 AND los<0) THEN
-        UPDATE Wizyty SET czy_odbyta = 'Odwołana', Opis = 'Pacjent nie zjawił się na wizycie.' WHERE wizyty.data_wizyty < SYSDATE AND wizyty.czy_odbyta NOT LIKE 'Odbyta' ;
-    END IF;
-END LOOP;
-    UPDATE Wizyty SET czy_odbyta = 'Odbyta' WHERE wizyty.data_wizyty < SYSDATE AND wizyty.czy_odbyta NOT LIKE 'Odwołana';
+    ZaplanowaneOpis := Opisarray('Nasilony ból głowy w ostatnich dniach',  'Dziwny wyprysk na wardze',  'Irytujące swędzenie we włosach',  'Ból zęba',  'Ucisk w klatce piersiowej',  'Kołatanie serca',  'Trudności w oddychaniu',  'Przemęczenie studenckie',  'Stany lękowe',  'Chroniczna depresja',  'Coroczna kontrola', '');
+    OdwolaneOpis := Opisarray('Pacjent odwołał wizytę',  'Pacjent odwołał wizytę',  'Pacjent odwołał wizytę',  'Pacjent odwołał wizytę',  'Problem sam się rozwiązał',  'Pacjent się nie stawił',  'Lekarz odwołał wizytę',  'Lekarz odwołał wizytę',  'Już się czuje lepiej',  '');
+    OdbyteOpis := Opisarray('Pacjent został wyleczony',  'Pacjent zmarł',  'Problem zażegnany',  'Zalecana dalsza hospitalizacja',  'Zalecane zgłoszenie się do szpitala',  '');
+    PrzeniesioneOpis := Opisarray('Choroba lekarza',  'Urlop lekarza',  'Brak funduszy z NFZ',  'Kwarantanna Covidowa',  'Brak wolnych miejsc',  '');
+    Zalecenia := Opisarray('Zarzywać jak najwięcej',  'Trzy razy dziennie',  'Brać tylko na noc',  'Brać po posiłkach',  'Brać przed posiłkami',  'Dawkować co tydzień');
+    FOR i IN 1..p_ilosc LOOP
+        prawdopodob:= dbms_random.VALUE(0, 99);
+        IF(prawdopodob >= 0 AND prawdopodob < 50) THEN
+            los:=round(dbms_random.VALUE(1, 100));
+            SELECT to_char(sysdate + los, 'DD.MM.YYYY') INTO dni FROM dual;
+            SELECT * INTO godziny FROM(SELECT * FROM dostepne_godz ORDER BY DBMS_RANDOM.RANDOM) WHERE  ROWNUM < 2;
+            data_wizyty := (dni||' '||godziny);
+            INSERT INTO Wizyty (lekarz_nr, pacjent_nr, Data_wizyty, czy_odbyta, Opis) VALUES(round(DBMS_RANDOM.VALUE(1,(SELECT MAX(Nr_Lekarza) FROM Lekarze))),round(DBMS_RANDOM.VALUE(1,(SELECT MAX(Nr_Karty_Pacjenta) FROM Pacjenci))), TO_DATE(data_wizyty,'DD.MM.YYYY HH24:MI'), 'Zaplanowana', ZaplanowaneOpis(dbms_random.VALUE(1, 12)));
+        ELSIF (prawdopodob >= 50 AND prawdopodob < 90) THEN
+            los:=round(dbms_random.VALUE(-360, -1));
+            SELECT to_char(sysdate + los, 'DD.MM.YYYY') INTO dni FROM dual;
+            SELECT * INTO godziny FROM(SELECT * FROM dostepne_godz ORDER BY DBMS_RANDOM.RANDOM) WHERE  ROWNUM < 2;
+            data_wizyty := (dni||' '||godziny);
+            INSERT INTO Wizyty (lekarz_nr, pacjent_nr, Data_wizyty, czy_odbyta, Opis) VALUES(round(DBMS_RANDOM.VALUE(1,(SELECT MAX(Nr_Lekarza) FROM Lekarze))),round(DBMS_RANDOM.VALUE(1,(SELECT MAX(Nr_Karty_Pacjenta) FROM Pacjenci))), TO_DATE(data_wizyty,'DD.MM.YYYY HH24:MI'), 'Odbyta', OdbyteOpis(dbms_random.VALUE(1, 6)));
+            IF(dbms_random.VALUE(0, 99) < 75) THEN
+                INSERT INTO Recepty (Wizyta_Nr, Zalecenia) VALUES ((SELECT MAX(NR_Wizyty) FROM Wizyty), Zalecenia(dbms_random.VALUE(1, 6)));
+                prawdopodob:=round(dbms_random.VALUE(1,4));
+                FOR i IN 1..prawdopodob LOOP
+                    INSERT INTO Lek_Na_Recepte VALUES ((SELECT MAX(NR_Recepty) FROM Recepty), (round(dbms_random.VALUE(1,(SELECT MAX(NR_LEKU) FROM LEKI)))));
+                END LOOP;
+            END IF;
+        ELSIF (prawdopodob >= 90 AND prawdopodob < 95) THEN
+            los:=round(dbms_random.VALUE(-360, -1));
+            SELECT to_char(sysdate + los, 'DD.MM.YYYY') INTO dni FROM dual;
+            SELECT * INTO godziny FROM(SELECT * FROM dostepne_godz ORDER BY DBMS_RANDOM.RANDOM) WHERE  ROWNUM < 2;
+            data_wizyty := (dni||' '||godziny);
+            INSERT INTO Wizyty (lekarz_nr, pacjent_nr, Data_wizyty, czy_odbyta, Opis) VALUES(round(DBMS_RANDOM.VALUE(1,(SELECT MAX(Nr_Lekarza) FROM Lekarze))),round(DBMS_RANDOM.VALUE(1,(SELECT MAX(Nr_Karty_Pacjenta) FROM Pacjenci))), TO_DATE(data_wizyty,'DD.MM.YYYY HH24:MI'), 'Odwołana', OdwolaneOpis(dbms_random.VALUE(1, 10)));
+        ELSIF (prawdopodob >= 95 AND prawdopodob < 100) THEN
+            los:=round(dbms_random.VALUE(1, 60));
+            SELECT to_char(sysdate + los, 'DD.MM.YYYY') INTO dni FROM dual;
+            SELECT * INTO godziny FROM(SELECT * FROM dostepne_godz ORDER BY DBMS_RANDOM.RANDOM) WHERE  ROWNUM < 2;
+            data_wizyty := (dni||' '||godziny);
+            INSERT INTO Wizyty (lekarz_nr, pacjent_nr, Data_wizyty, czy_odbyta, Opis) VALUES(round(DBMS_RANDOM.VALUE(1,(SELECT MAX(Nr_Lekarza) FROM Lekarze))),round(DBMS_RANDOM.VALUE(1,(SELECT MAX(Nr_Karty_Pacjenta) FROM Pacjenci))), TO_DATE(data_wizyty,'DD.MM.YYYY HH24:MI'), 'Przeniesiona', PrzeniesioneOpis(dbms_random.VALUE(1, 6)));
+        END IF;
+    END LOOP;
 END;
 /
-EXECUTE dodaj_wizyte_random(200);
+SET SERVEROUTPUT ON;
+EXECUTE dodaj_wizyte_random(700);
 ALTER TRIGGER check_Wizyty_dates_trigger ENABLE;
