63be0c1d64b5ef055810a98ae49885de1093667f
ObalonyPiwot <74457736+ObalonyPiwot@users.noreply.github.com>
2022-01-21 17:41:45 +0100

    Update CREATE.sql

--- SQL/CREATE.sql
+++ SQL/CREATE.sql
@@ -82,7 +82,8 @@ CREATE TABLE Recepty(
     Wizyta_Nr Number NOT NULL,
     Data_Wystawienia DATE  DEFAULT SYSDATE,
     Data_Waznosci DATE  DEFAULT SYSDATE+30,
-    Zalecenia NVARCHAR2(256)
+    Zalecenia NVARCHAR2(256),
+    CONSTRAINT Wizyta_fk FOREIGN KEY(Wizyta_Nr) REFERENCES Wizyty(Nr_Wizyty)
 );
 	
 CREATE TABLE Lekarze(
